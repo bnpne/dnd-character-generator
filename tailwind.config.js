b2c571c14d4405123df45beba2c4e6eac0947f7b
bnpne <bentppaine@gmail.com>
2021-02-21 11:48:26 -0800

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: [
+        'Inter var',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
